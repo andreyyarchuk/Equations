6dfc58c8213904bfa3f177f09a8cbc77fc30b0c2
Andrey <dron4432@gmail.com>
2020-08-22 22:18:00 +0200

    add functions

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-let play;
-let sign;
+// объявление переменных
+let firstValue;
+let secondValue;
+let valueForSing;
+let a = 2;
 
-let firstNumber = Math.floor( Math.random() * 10 );
+// Функции
 
-let secondNumber = Math.floor( Math.random() * 10 );
+function sum(firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
+}
+
+function sub(firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
+}
+
+function sing(a) {
+    return Math.floor(Math.random() * Math.floor(a));
+}
+// Алгоритм
+
+firstNumber = Math.floor( Math.random() * 10 );
+
+secondNumber = Math.floor( Math.random() * 10 );
 
 let numberForSign = Math.floor(Math.random() * Math.floor(2));
 
@@ -16,4 +34,5 @@ if (numberForSign == 0) {
 do {
     play = confirm("Will play?");
     alert("How much will: " + ` ${firstNumber}` +  ` ${sign} `  + `${secondNumber}` + " =");
-} while (play == true);
+} while (play == true); fa
+
